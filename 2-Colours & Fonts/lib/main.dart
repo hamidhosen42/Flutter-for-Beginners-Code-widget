d7752e7116cd72689851236fc6ff41de15db2b2e
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-10-07 23:01:32 +0600

    Add files via upload

---
+++ 2-Colours & Fonts/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// import 'package:flutter/src/material/dropdown.dart';
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        //title:Center(child: Text("Hamid Hosen")),
+        title: Text(
+          "Hamid Hosen",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            fontFamily: "Ephesis",
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+          child: Text(
+        "Hello Hamid !",
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          fontFamily: "Ephesis",
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Text(
+          "Click",
+        ),
+        onPressed: () {},
+        backgroundColor: Colors.orange,
+      ),
+    ),
+  ));
+}
