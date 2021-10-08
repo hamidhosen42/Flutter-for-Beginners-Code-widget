1be9027b9c0bc66cab26aa43e5b376312827f632
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-10-08 10:10:48 +0600

    Add files via upload

---
+++ 9-Flutter Outline & Shortcuts/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+// import 'package:flutter/src/material/dropdown.dart';
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //title:Center(child: Text("Hamid Hosen")),
+        title: Text(
+          "Hamid Hosen",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            fontFamily: "Ephesis",
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        // //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // //mainAxisAlignment: MainAxisAlignment.start,
+        // //mainAxisAlignment: MainAxisAlignment.end,
+        // //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+        // //crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        // //crossAxisAlignment: CrossAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Text("Two"),
+            color: Colors.green,
+          ),
+          Container(
+            padding: EdgeInsets.all(40),
+            child: Text("Three"),
+            color: Colors.red,
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text("One"),
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text(
+          "Click",
+        ),
+        onPressed: () {},
+        backgroundColor: Colors.orange,
+      ),
+    );
+  }
+}
