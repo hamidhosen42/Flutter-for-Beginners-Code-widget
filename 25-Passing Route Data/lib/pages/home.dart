24d6c0ae6ca9fb241ab022bae872a0e826434b41
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-10-11 23:06:38 +0600

    Add files via upload

---
+++ 25-Passing Route Data/lib/pages/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  Map data = {};
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context).settings.arguments;
+    print(data);
+
+    return Scaffold(
+      //appBar: AppBar(),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: Icon(Icons.edit_location),
+              label: Text(
+                'Edit Location'
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
